0eb6b11b8b5ada1993e0bdf12525408399a527d8
Lazy Cat <mylazysundaycat@gmail.com>
2024-02-23 17:45:15 +0900

    섹션7 상품 수정

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -21,22 +21,22 @@ public abstract class Item {
 
     private String name;
     private int price;
-    private int stockQunatity;
+    private int stockQuantity;
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
 
     //==비즈니스 로직==//
     public void addStock(int quantity) {
-        this.stockQunatity += quantity;
+        this.stockQuantity += quantity;
     }
 
     public void removeStock(int quantity) {
-        int restStock = this.stockQunatity - quantity;
+        int restStock = this.stockQuantity - quantity;
         if (restStock < 0) {
             throw new NotEnoughStockException("nee more stock");
         }
-        this.stockQunatity = restStock;
+        this.stockQuantity = restStock;
     }
 
 
